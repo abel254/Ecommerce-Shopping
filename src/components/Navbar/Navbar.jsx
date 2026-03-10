04e47ca38c5bbd47167a0bc9c025849bf6384f4d
abel254 <abelmuema353@gmail.com>
2026-03-11 01:41:33 +0300

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,53 @@ import logo from "../../assets/shopify-logo.png";
 import { IoMdSearch } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
 import DarkMode from "./DarkMode";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#"
+  },
+   {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services"
+  },
+   {
+    id: 3,
+    name: "Kid's Wear",
+    link: "/#"
+  },
+   {
+    id: 4,
+    name: "Men's Wear",
+    link: "/#"
+  },
+   {
+    id: 5,
+    name: "Electronics",
+    link: "/#"
+  },
+]
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "#"
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "#"
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "#"
+  },
+]
 
 const Navbar = () => {
   return (
@@ -22,7 +69,7 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="search"
-                className="w-50 sm:w-50 bg-white group-hover:w-75 transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border focus:border-primary"
+                className="w-50 sm:w-50 bg-white group-hover:w-75 transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border focus:border-primary dark:border-gray-500 dark:bg-gray-800"
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
@@ -45,7 +92,33 @@ const Navbar = () => {
         </div>
       </div>
       {/* Lower Nav */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a href={data.link} className="inline-block px-4 hover:text-primary duration-200">{data.name}</a>
+            </li>
+          ))}
+          {/* Simple Dropdown and Links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-0.5 py-2"> 
+              Trending Products
+              <span>
+                <IoMdArrowDropdown className="transition-all duration-200 group-hover:rotate-180"/>
+              </span>
+              </a>
+              <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
+                <ul>
+                  {DropdownLinks.map((data) => (
+                    <li key={data.id}>
+                      <a href={data.link} className="inline-block w-full rounded-md p-2 hover:bg-primary/20">{data.name}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
