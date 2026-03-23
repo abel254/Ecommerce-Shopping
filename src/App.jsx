07a2e37119730dc9f879dae1d22b8422821a46be
abel254 <abelmuema353@gmail.com>
2026-03-23 09:48:22 +0300

    banner and subscription

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Products from './components/Products/Products'
 import Aos from 'aos'
 import "aos/dist/aos.css"
 import TopProducts from './components/TopProducts/TopProducts'
+import Banner from './components/Banner/Banner'
+import Subscribe from './components/Subscribe/Subscribe'
 
 const App = () => {
 
@@ -24,6 +26,8 @@ const App = () => {
       <Hero/>
       <Products/>
       <TopProducts/>
+      <Banner/>
+      <Subscribe/>
 
       <h1 className='text-center py-3 mt-20 font-bold text-3xl'>Development in Progress...</h1>
     </div>
---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Banner from "../../assets/website/orange-background.avif";
+
+const Subscribe = () => {
+  const BannerImg = {
+    backgroundImage: `url(${Banner})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+  };
+
+  return (
+    <div
+      data-aos="zoom-in"
+      className="mb-20 bg-gray-100 dark:bg-gray-800 text-white"
+      style={BannerImg}
+    >
+      <div className="container backdrop-blur-sm py-10">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
+            Get Notified About New Products
+          </h1>
+          <input
+            data-aos="fade-up"
+            type="text"
+            className="w-full p-3 bg-white text-gray-800"
+            placeholder="Enter your email"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
